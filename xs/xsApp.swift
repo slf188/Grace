08964fc71a15f5241e842c9c683e376c9db79757
Felipe Vallejo <stephanovallejom@duck.com>
2023-11-07 23:18:19 -0500

    Initial Commit

---
+++ xs/xsApp.swift
@@ -0,0 +1,17 @@
+//
+//  xsApp.swift
+//  xs
+//
+//  Created by Felipe Vallejo on 7/11/23.
+//
+
+import SwiftUI
+
+@main
+struct xsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
